5e30ed0cc842ec4bec352668be6771118b01ccc8
mason-carreon <70252484+mason-carreon@users.noreply.github.com>
2022-05-09 18:11:22 -0500

    Update app.js

--- app.js
+++ app.js
@@ -1,8 +1,8 @@
 const PaintMeAPrettyPicture = new Audio();
-PaintMeAPrettyPicture.src = "/audio/Paint Me A Pretty Picture.wav";
+PaintMeAPrettyPicture.src = "./audio/Paint Me A Pretty Picture.wav";
 
 const HeartbreakSoldier = new Audio();
-HeartbreakSoldier.src = "/audio/Heartbreak Soldier.wav";
+HeartbreakSoldier.src = "./audio/Heartbreak Soldier.wav";
 
 const Confessions = new Audio();
-Confessions.src = "/audio/Confessions.wav";
+Confessions.src = "./audio/Confessions.wav";
